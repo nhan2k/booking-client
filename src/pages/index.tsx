321fdeebe06a2f541160aa92002ce9de265bdd83
nhan2k <nhanrooney112@gmail.com>
2023-04-19 22:39:54 +0700

    fixed user cannot create hotel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -76,30 +76,34 @@ function Page() {
               </defs>
             </svg>
           </div>
-          <div className="text-center mt-4">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              List your property
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              Reach the guests you want—those who truly value your
-              property—with Expedia Group. Signing up is free, fast,
-              and easy.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <Link
-                href="hotel/post"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </Link>
-              <Link
-                href="#"
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
-                Learn more <span aria-hidden="true">→</span>
-              </Link>
+          {(session?.user as any)?.role === 'hotelier' ? (
+            <div className="text-center mt-4">
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                List your property
+              </h1>
+              <p className="mt-6 text-lg leading-8 text-gray-600">
+                Reach the guests you want—those who truly value your
+                property—with Expedia Group. Signing up is free, fast,
+                and easy.
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6">
+                <Link
+                  href="hotel/post"
+                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Get started
+                </Link>
+                <Link
+                  href="#"
+                  className="text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Learn more <span aria-hidden="true">→</span>
+                </Link>
+              </div>
             </div>
-          </div>
+          ) : (
+            <React.Fragment />
+          )}
           {(session?.user as any)?.role !== 'hotelier' ? (
             <div className="mx-auto max-w-7xl py-20">
               <div className="hidden sm:mb-8 sm:flex sm:justify-center">
